08bf52ee8ef6975a7a523c2075be111395893844
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2026-01-23 10:52:36 +0200

    adde features tab 3

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import SimpleBookmarking from './features/SimpleBookmarking'
 import SpeedySearching from './features/SpeedySearching'
+import EasySharing from './features/EasySharing'
 
 const Features = () => {
     const [tab, setTab] = useState(1)
@@ -13,7 +14,7 @@ const Features = () => {
             <h3 onClick={() => setTab(2)} className={`${tab === 2 ? 'after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:w-[40%] after:md:w-full after:h-1 after:bg-red-700 after:content-[""]' : ''} py-3 cursor-pointer border-t border-gray-400 relative w-full md:border-t-0 hover:text-red-700 md:border-b md:w-fit md:px-6`}>Speedy Searching</h3>
             <h3 onClick={() => setTab(3)} className={`${tab === 3 ? 'after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:w-[40%] after:md:w-full after:h-1 after:bg-red-700 after:content-[""]' : ''} py-3 cursor-pointer border-y border-gray-400 relative w-full md:border-t-0 hover:text-red-700 md:w-fit md:px-6`}>Easy Sharing</h3>
         </nav> 
-        {tab === 1 ? <SimpleBookmarking /> : <SpeedySearching /> }
+        {tab === 1 ? <SimpleBookmarking /> : tab === 2 ? <SpeedySearching /> : <EasySharing /> }
     </div>
   )
 }
