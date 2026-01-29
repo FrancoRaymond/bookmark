50abd9ce1672f63c0add82e2bef4a68d08290034
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2026-01-29 18:39:37 +0200

    complete footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,14 @@ import twitter from '../assets/images/icon-twitter.svg'
 
 const Footer = () => {
   return (
-    <div className='bg-[#242946]'>
-        <img src={logo} alt="" />
-        <ul className='text-white text-sm'>
+    <div className='bg-[#242946] py-6 flex flex-col gap-6 sm:gap-10 sm:flex-row sm:mx-0 px-2 sm:px-5 md:px-10 lg:px-24'>
+        <img src={logo} alt="" className='w-44 mx-auto sm:mx-0 sm:h-7'/>
+        <ul className='text-white text-sm mx-auto flex flex-col gap-4 sm:flex-row sm:mx-0 '>
             <li><a href="#">FEATURES</a></li>
             <li><a href="#">PRICING</a></li>
             <li><a href="#">CONTACT</a></li>
         </ul>
-        <ul className='flex items-center gap-6 mt-auto mx-auto'>
+        <ul className='flex items-center gap-6 mt-5 mx-auto sm:mx-0 sm:mt-0 sm:ml-auto'>
             <li><a href="#"><img src={facebook} alt="" /></a></li>
             <li><a href="#"><img src={twitter} alt="" /></a></li>
         </ul>
